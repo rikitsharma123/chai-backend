c44ca6473b74f67e4983434e1219bedb7ff2296a
Rikit Sharma <rikitsharma176@gmail.com>
2026-02-23 22:04:52 +0530

    User Schema and Video Schema

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      require: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    duration: {
+      type: String,
+      require: true,
+    },
+    views: {
+      type: Number,
+      require: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
